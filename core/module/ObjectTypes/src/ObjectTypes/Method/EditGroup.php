9064f2ee8b14e37aac0ab9520a4407fd66f16933
ildar <ildar.programist@gmail.com>
2013-09-16 05:18:54 +0600

    рефакторинг

--- core/module/ObjectTypes/src/ObjectTypes/Method/EditGroup.php
+++ core/module/ObjectTypes/src/ObjectTypes/Method/EditGroup.php
@@ -42,45 +42,15 @@ class EditGroup extends AbstractMethod
             if ($form->isValid()) {
                 $data = $form->getData();
                 
-                $result['groupId'] = $objectType->addFieldsGroup($data['name'], $data['title']);
+                $result['groupId'] = $fieldsGroup->setName($data['name'])
+                                                 ->setTitle($data['title'])
+                                                 ->save();
                 $result['name'] = $data['name'];
                 $result['title'] = $data['title'];
                 $result['success'] = true;
                 $result['msg'] = 'Группа успешно создана';
             } else {
                 $result['success'] = false;
-                $result['formMsg'] = $form->getMessages();
-            }
-        }
-        return $result;
-    }
-}
-
-class EditGroup2 extends AbstractMethod
-{    
-    public function main()
-    {
-        $objectTypesModel = new ObjectTypesModel($this->serviceLocator);
-        
-        $result = array(
-            'success' => 0,
-        );
-        if (null === $this->params()->fromRoute('id') || null === $this->params()->fromPost('name') || null === $this->params()->fromPost('title')) {
-            $result['errMsg'] = 'Не переданы все необходимые параметры';
-        } 
-        else {
-            $groupId = (int)$this->params()->fromRoute('id');
-            
-            $tmp = $objectTypesModel->editGroup($groupId, $this->params()->fromPost());
-            
-            if ($tmp['success']) {
-                $result['msg'] = 'Группа успешно обновлена';
-                $result['name'] = $tmp['name'];
-                $result['title'] = $tmp['title'];
-                $result['success'] = true;
-            } else {
-                $result['success'] = false;
-                $form = $tmp['form'];
                 $result['formMsg'] = $form->getMessages();
             }
         }
